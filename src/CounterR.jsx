8f961e4f7a5fb61e5970fc2f61895e293b69bae1
Aamir Aziz <aamiraziz.khattak@gmail.com>
2025-07-08 18:54:04 +0500

    code rewrite for useReducer function

--- src/CounterR.jsx
+++ src/CounterR.jsx
@@ -1,41 +1,44 @@
 /*
     * This file uses useReducer function to change counter value.
     * Dated: July 07, 2025.
-    * 
+    * useReducer is the advance of useState to handle multiple states easily
+    * it has a reducer function and initial state
+    * in reducer function it has state and action.
 */
 
-import React, {useReducer} from 'react'
+import {React, useReducer} from 'react'
 
 const initialState = {count: 0}
 
-function reducer(state, action) {
+function reducer (state, action) {
 
     switch (action.type) {
 
         case 'increment':
-            return {count: state.count + 1}
+            return { count: state.count + 1 }
         case 'decrement':
-            return {count: state.count - 1}
+            return { count: state.count - 1}
         case 'reset':
-            return {count: 0}
+            return { count: 0}
         default:
             throw new Error()
+
     }
 }
 
-function CounterR() {
+function CounterR () {
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
 
         <div>
-            <h1>Count: {state.count} </h1>
-            <button onClick={() => dispatch({type: 'increment'}) }>+</button>
-            <button onClick={() => dispatch({type: 'decrement'}) } >-</button>
-            <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+            <h1>Counts: {state.count} </h1>
+            
+            <button onClick={ () => dispatch( {type: 'increment'} ) } >+</button>
+            <button onClick={ () => dispatch( {type: 'decrement'} )} >-</button>
+            <button onClick={ () => dispatch( {type: 'reset'} ) } >Reset</button>
         </div>
-
 
     )
 
